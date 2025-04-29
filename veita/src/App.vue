887be9a1076f7e1679ecc94677581a072637a1ee
sakots <sakots8000@gmail.com>
2025-04-30 06:27:53 +0900

    パーツ分割

--- veita/src/App.vue
+++ veita/src/App.vue
@@ -15,14 +15,18 @@
       </section>
     </div>
   </main>
+  <footer id="footer">
+    <footerItem />
+  </footer>
 </template>
 
 <script setup>
 
-import headerItem from './components/headerItem.vue'
-import pagingDefault from './components/pagingDefault.vue'
-import viewDefault from './components/viewDefault.vue'
-import itemSearch from './components/itemSearch.vue'
-import deleteForm from './components/deleteForm.vue'
+  import headerItem from './components/headerItem.vue'
+  import pagingDefault from './components/pagingDefault.vue'
+  import viewDefault from './components/viewDefault.vue'
+  import itemSearch from './components/itemSearch.vue'
+  import deleteForm from './components/deleteForm.vue'
+  import footerItem from './components/footerItem.vue'
 
 </script>
---
+++ veita/src/components/footerItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="headers" class="copy">
+    <!-- 著作権表示 -->
+    <p>
+      <a href="https://dev.oekakibbs.net/" target="_blank">V-eita {{ headers.ver }}</a>
+    </p>
+    <p>
+      OekakiApplet -
+      <a href="https://github.com/funige/neo/" target="_blank" rel="noopener noreferrer" title="by funige">PaintBBS NEO</a>
+      <span v-if="headers.use_chicken === 1"> ,<a href="https://github.com/satopian/ChickenPaint_Be" target="_blank" rel="nofollow noopener noreferrer" title="by Nicholas Sherlock">ChickenPaint Be</a></span>
+    </p>
+    <p>
+      UseFunction -
+      <!-- http://wondercatstudio.com/ -->DynamicPalette,
+      <a href="https://huruihone.tumblr.com/" target="_blank" rel="noopener noreferrer" title="by Soto">AppletFit</a>
+    </p>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { ref, onMounted } from 'vue';
+
+const headers = ref()
+const targetUrl = import.meta.env.VITE_BASE_URL + '?mode=get_header'
+const getHeaders = () => {
+  axios.get(targetUrl).then((res) => {
+    headers.value = res.data
+  })
+}
+//DOM読み込み後に展開する
+onMounted(async () => {
+  getHeaders()
+})
+</script>
